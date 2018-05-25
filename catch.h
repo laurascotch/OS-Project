daa65cf8a623bdb51546ef699959fca24a068e5b
TheUnknow23 <32764086+TheUnknow23@users.noreply.github.com>
2018-05-25 17:18:41 +0200

    Clean

--- catch.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-void stampa_cmd(char *str_cmd, struct command *buf, int index);
-void esegui(struct command *buf);
-void pipeHandler(struct command *buf, int index);
-
-#endif
