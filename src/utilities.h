31c2dc7b86ecc7b152fbacd2e38809ba3a882f54
TheUnknow23 <matteo.longato@studenti.unitn.it>
2018-05-26 19:18:42 +0200

    Modified Makefile info

--- src/utilities.h
+++ src/utilities.h
@@ -9,8 +9,9 @@ typedef struct command{
 
 int aiuto();
 int funz_cd(struct command *buf);
-void argCheck(int argc, char **argv, char *outputPath, char *errPath, int *code);
+void argCheck(int argc, char **argv, char *outputPath, char *errPath, int *code, int *maxlen);
 void fileOpen(char *outputPath, char *errPath, int *oF, int *eF);
 struct command *readline(int *index);
+int min(int a, int b);
 
 #endif
